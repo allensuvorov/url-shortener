9ab6e788ecea706154267e5ccd8013af5207543d
Allen <allenvs@mail.ru>
2022-08-27 21:52:43 +0300

    POST reads URL from body

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,12 +1,28 @@
 package main
 
 import (
+	"io"
 	"net/http"
 )
 
 // HelloWorld — обработчик запроса.
 func HelloWorld(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("<h1>Hello, World</h1>"))
+	if r.URL.Path != "/" {
+		http.Error(w, "404 not found.", http.StatusNotFound)
+		return
+	}
+	if r.Method == "POST" {
+		// читаем Body
+		b, err := io.ReadAll(r.Body)
+		// обрабатываем ошибку
+		if err != nil {
+			http.Error(w, err.Error(), 500)
+			return
+		}
+		w.Write(b)
+	} else {
+		w.Write([]byte("<h1>Hello, World</h1>"))
+	}
 }
 
 func main() {
