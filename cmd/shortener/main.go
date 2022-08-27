11a0172e37f90c94beb4f662e2f400e7eb536b3b
Allen <allenvs@mail.ru>
2022-08-27 20:24:44 +0300

    default server code

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,17 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+)
+
+// HelloWorld — обработчик запроса.
+func HelloWorld(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<h1>Hello, World</h1>"))
+}
+
+func main() {
+	// маршрутизация запросов обработчику
+	http.HandleFunc("/", HelloWorld)
+	// запуск сервера с адресом localhost, порт 8080
+	http.ListenAndServe(":8080", nil)
+}
