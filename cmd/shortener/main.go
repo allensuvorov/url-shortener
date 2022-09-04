47026c721b956e782113041e69b974c323c57d63
Allen <allenvs@mail.ru>
2022-09-04 17:49:11 +0300

    getUniqShortHash in util.go

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,122 +1,14 @@
 package main
 
 import (
-	"crypto/sha256"
-	"fmt"
-	"io"
 	"log"
 	"net/http"
-	"net/url"
-	"path"
+	"yandex/projects/urlshortner/internal/app/shortner/handlers"
 )
-
-// map to store short urls and full urls
-var urls map[string]string = make(map[string]string)
-
-// sha256 to generate the hash value
-func Shorten(s string) string {
-	h := sha256.New()
-	h.Write([]byte(s))
-	// log.Println(fmt.Sprintf("%x", h.Sum(nil)))
-
-	hash := fmt.Sprintf("%x", h.Sum(nil))
-	var shortURL string
-
-	// check if short URL is already in the map for a different long url, expand hash slice till unique
-	for i := 8; i < len(hash); i++ {
-		shortURL = hash[0:i]
-		if v, ok := urls[shortURL]; !(ok && v != s) {
-			break
-		}
-	}
-	log.Println("created new shortURL", shortURL)
-	return shortURL
-}
-
-// CreateShortURL — обработчик запроса.
-func Handler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		log.Println("path is /", r.URL.Path)
-		if r.Method == "GET" {
-
-			// get part after last slash
-			base := path.Base(r.URL.Path)
-			log.Println("after last slash", base)
-			log.Println(r.URL, r.URL.Path)
-
-			// check if hash exists
-			if _, ok := urls[base]; !ok {
-				http.Error(w, "URL does not exist", 400)
-				return
-			}
-
-			// set header Location
-			w.Header().Set("Location", urls[base])
-
-			// устанавливаем статус-код 307
-			w.WriteHeader(http.StatusTemporaryRedirect)
-
-			w.Write([]byte(urls[base]))
-
-			return
-		}
-	}
-	if r.Method == "POST" {
-		// читаем Body
-		b, err := io.ReadAll(r.Body)
-		// обрабатываем ошибку
-		if err != nil {
-			http.Error(w, err.Error(), 500)
-			return
-		}
-		// check if long URL is empty string
-		if len(b) == 0 {
-			http.Error(w, "empty URL", 400)
-			return
-		}
-		// check it URL is valid
-		u, err := url.ParseRequestURI(string(b))
-		log.Println("parsed URL", u)
-		if err != nil {
-			http.Error(w, err.Error(), 400)
-			//log.Printf("hi/there?: err=%+v url=%+v\n", err, u)
-			return
-		}
-
-		// check if long url is already in the map
-		var exists bool
-		var shortURL string
-		for k, v := range urls {
-			if v == string(b) {
-				exists = true
-				shortURL = k
-				break
-			}
-		}
-
-		if !exists {
-
-			// get shortened URL
-			shortURL = Shorten(string(b))
-
-			// add url to the map
-			urls[shortURL] = string(b)
-		}
-
-		// устанавливаем статус-код 201
-		w.WriteHeader(http.StatusCreated)
-
-		shortURL = "http://localhost:8080/" + shortURL
-
-		// пишем тело ответа
-		w.Write([]byte(shortURL))
-	}
-
-}
 
 func main() {
 	// маршрутизация запросов обработчику
-	http.HandleFunc("/", Handler)
+	http.HandleFunc("/", handlers.Multiplexer)
 	// запуск сервера с адресом localhost, порт 8080
 	log.Fatal(http.ListenAndServe(":8080", nil)) // log.Fatal will print errors if server crashes
 }
--- internal/app/shortner/util/util.go
+++ internal/app/shortner/util/util.go
@@ -11,18 +11,25 @@ import (
 // sha256 to generate the hash value
 func Shorten(s string) string {
 	h := sha256.New()
-	h.Write([]byte(s))
+	h.Write([]byte(s)) // what's that?
 
 	hash := fmt.Sprintf("%x", h.Sum(nil))
-	var sh string // short hash
+	sh := getUniqShortHash(hash, s)
+	log.Println("created new shortURL", sh)
+	return sh
+}
 
-	// check if short URL (hash) is already in the map for a different long url, expand hash slice till unique
-	for i := 8; i < len(hash); i++ {
-		sh = hash[0:i]
-		if storage.HashExists(sh) && storage.GetURL(sh) != s {
-			break
+// check if short URL (hash) is already in the map for a different long url, expand hash slice till unique
+func getUniqShortHash(h string, s string) string {
+	var sh string // short hash
+	for i := 8; i < len(h); i++ {
+		sh = h[0:i]
+		if !storage.HashExists(sh) {
+			return sh
+		}
+		if storage.GetURL(sh) == s {
+			return sh
 		}
 	}
-	log.Println("created new shortURL", sh)
 	return sh
 }
