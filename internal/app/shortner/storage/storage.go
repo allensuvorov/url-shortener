4c19cb5160948755d43b87ccd5f323bf39c19b40
Allen <allenvs@mail.ru>
2022-09-04 13:18:15 +0300

    listed methods need for storage

--- internal/app/shortner/storage/storage.go
+++ internal/app/shortner/storage/storage.go
@@ -2,3 +2,9 @@ package storage
 
 // map to store short urls and full urls
 var Urls map[string]string = make(map[string]string)
+
+// methods we need here:
+// add new record - pair shortURL: longURL
+// return longURL for the matching shortURL
+// check if shortURL exists
+// check if longURL exists
