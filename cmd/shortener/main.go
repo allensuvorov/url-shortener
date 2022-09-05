c783853a1c48e068289a05f6d2fc0154a9075bdb
Allen <allenvs@mail.ru>
2022-09-05 15:50:25 +0300

    updated import to ...chi/v5, started server with chi (which is super strong;)

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,16 +1,24 @@
 package main
 
 import (
-	"log"
+	"fmt"
 	"net/http"
 
 	"github.com/allensuvorov/urlshortner/internal/app/shortner/handlers"
+	"github.com/go-chi/chi/v5"
 )
 
 func main() {
 	r := chi.NewRouter()
+
+	r.Post("/", handlers.PostHandler)
+	r.Get("/", handlers.GetHandler)
+
+	fmt.Println("Serving on port: 8080")
+	http.ListenAndServe(":8080", r)
+
 	// маршрутизация запросов обработчику
-	http.HandleFunc("/", handlers.Multiplexer)
-	// запуск сервера с адресом localhost, порт 8080
-	log.Fatal(http.ListenAndServe(":8080", nil)) // log.Fatal will print errors if server crashes
+	// http.HandleFunc("/", handlers.Multiplexer)
+	// // запуск сервера с адресом localhost, порт 8080
+	// log.Fatal(http.ListenAndServe(":8080", nil)) // log.Fatal will print errors if server crashes
 }
