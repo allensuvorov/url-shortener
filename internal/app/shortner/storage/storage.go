3a147e0eb49c4fedc7994071b535ee4aac6f0c0e
Allen <allenvs@mail.ru>
2022-09-12 20:46:36 +0300

    storage.go with 3 methods ready

--- internal/app/shortner/storage/storage.go
+++ internal/app/shortner/storage/storage.go
@@ -1,15 +1,12 @@
 package storage
 
 import (
-	"log"
 	"yandex/projects/urlshortner/internal/app/shortner/domain/entity"
+	"yandex/projects/urlshortner/internal/app/shortner/domain/errors"
 
 	"github.com/allensuvorov/urlshortner/internal/app/shortner/domain/entity"
-	//"github.com/allensuvorov/urlshortner/internal/app/domain/entity"
+	"github.com/allensuvorov/urlshortner/internal/app/shortner/domain/errors"
 )
-
-// map to store short urls and full urls
-// var Urls map[string]string = make(map[string]string)
 
 // Object with storage methods to work with DB
 type URLStorage struct {
@@ -38,48 +35,11 @@ func (us URLStorage) GetByURL(u string) (entity.URLEntity, error) {
 	return nil, errors.NotFound
 }
 
-// Get URL record by URL or Hash
-func (us URLStorage) Get(ur entity.URLEntity) (entity.URLEntity, error) {
-	if len(ur.URL) != 0 {
-		for _, v := range us.inMemory {
-			if v.URL == ur.URL {
-				return v
-			}
+func (us URLStorage) GetByHash(u string) (entity.URLEntity, error) {
+	for _, v := range us.inMemory {
+		if v.Hash == u {
+			return v, nil
 		}
 	}
-	if len(ur.Hash) != 0 {
-		for _, v := range us.inMemory {
-			if v.Hash == ur.Hash {
-				return v
-			}
-		}
-	}
-	return ur, nil
-}
-
-// HashExists checks if hash exists.
-func HashExists(h string) bool {
-	if _, ok := Urls[h]; ok {
-		return true
-	}
-	return false
-}
-
-// GetHash returns hash for the original longURL.
-func GetHash(u string) (string, bool) {
-	for k, v := range Urls {
-		if v == u {
-			log.Println("URL already exists")
-			return k, true
-		}
-	}
-	return "", false
-}
-
-// GetURL returns longURL for the matching hash.
-func GetURL(h string) (string, bool) {
-	if u, ok := Urls[h]; ok {
-		return u, true
-	}
-	return "", false
+	return nil, errors.NotFound
 }
