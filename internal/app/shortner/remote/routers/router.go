2d171050a376119ffa0c9c9a3c034351d4a1c702
Allen <allenvs@mail.ru>
2022-09-12 05:28:28 +0300

    create handler.go in handlers/url

--- internal/app/shortner/remote/routers/router.go
+++ internal/app/shortner/remote/routers/router.go
@@ -1,6 +1,9 @@
 package routers
 
-import "github.com/go-chi/chi/v5"
+import (
+	"github.com/allensuvorov/urlshortner/internal/app/shortner/remote/handlers/url"
+	"github.com/go-chi/chi/v5"
+)
 
 func NewRouter(url url.URLHandler) chi.Router {
 	r := chi.NewRouter()
