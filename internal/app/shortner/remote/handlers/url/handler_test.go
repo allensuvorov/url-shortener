66162b0c6245f1cb02f0182a6c8a1de87fc687fa
Allen <allenvs@mail.ru>
2022-09-13 18:07:00 +0300

    building test for handler create

--- internal/app/shortner/remote/handlers/url/handler_test.go
+++ internal/app/shortner/remote/handlers/url/handler_test.go
@@ -1,8 +1,13 @@
 package url
 
 import (
+	"bytes"
 	"net/http"
+	"net/http/httptest"
 	"testing"
+
+	service "github.com/allensuvorov/urlshortner/internal/app/shortner/service/url"
+	"github.com/allensuvorov/urlshortner/internal/app/shortner/storage"
 )
 
 func TestURLHandler_Create(t *testing.T) {
@@ -13,12 +18,35 @@ func TestURLHandler_Create(t *testing.T) {
 		w http.ResponseWriter
 		r *http.Request
 	}
+
+	// Mock data
+	// Object
+	URLService := service.NewURLService(storage.NewURLStorage())
+
+	// Mock Request
+	jsonBody := []byte("http://www.apple.com/store")
+	bodyReader := bytes.NewReader(jsonBody)
+	requestURL := "localhost:8080/"
+
+	req, err := http.NewRequest("POST", requestURL, bodyReader)
+
+	if err != nil {
+		t.Fatalf("could not create request: %v", err)
+	}
+
+	rec := httptest.NewRecorder()
+
+	// test data
 	tests := []struct {
-		name   string
-		fields fields
-		args   args
+		name string
+		fields
+		args args
 	}{
-		// TODO: Add test cases.
+		{
+			name:   "apple store",
+			fields: fields{urlService: URLService},
+			args:   args{rec, req},
+		}, // TODO: Add test cases.
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
--- internal/app/shortner/remote/handlers/url/handlers_test.go
+++ internal/app/shortner/remote/handlers/url/handlers_test.go
@@ -1,80 +1,80 @@
-package url
+// package url
 
-import (
-	"bytes"
-	"io/ioutil"
-	"net/http"
-	"net/http/httptest"
-	"testing"
-)
+// import (
+// 	"bytes"
+// 	"io/ioutil"
+// 	"net/http"
+// 	"net/http/httptest"
+// 	"testing"
+// )
 
-func TestPostURL(t *testing.T) {
+// func TestPostURL(t *testing.T) {
 
-	jsonBody := []byte("http://www.apple.com/store")
-	bodyReader := bytes.NewReader(jsonBody)
-	requestURL := "localhost:8080/"
+// 	jsonBody := []byte("http://www.apple.com/store")
+// 	bodyReader := bytes.NewReader(jsonBody)
+// 	requestURL := "localhost:8080/"
 
-	req, err := http.NewRequest("POST", requestURL, bodyReader)
+// 	req, err := http.NewRequest("POST", requestURL, bodyReader)
 
-	if err != nil {
-		t.Fatalf("could not create request: %v", err)
-	}
-
-	rec := httptest.NewRecorder()
-	PostURL(rec, req)
-
-	res := rec.Result()
-	defer res.Body.Close()
-	if res.StatusCode != http.StatusCreated {
-		t.Errorf("expected status Created; got %v", res.Status)
-	}
-	b, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		t.Fatalf("coult not read respons: %v", err)
-	}
-	if string(b) != "http://localhost:8080/a7d59904" {
-		t.Fatalf("expected http://localhost:8080/a7d59904; got %s", string(b))
-	}
-}
-
-func TestGetURL(t *testing.T) {
-	// TODO: create a record for the the URL, to that this test can work independatly
-	requestURL := "http://localhost:8080/a7d59904"
-	req, err := http.NewRequest("GET", requestURL, nil)
-
-	if err != nil {
-		t.Fatalf("could not create request: %v", err)
-	}
-
-	rec := httptest.NewRecorder()
-	GetURL(rec, req)
-
-	res := rec.Result()
-	defer res.Body.Close()
-	if res.StatusCode != http.StatusTemporaryRedirect {
-		t.Errorf("expected status TemporaryRedirect; got %v", res.Status)
-	}
-
-	if res.Header.Get("Location") != "http://www.apple.com/store" {
-		t.Errorf("Expected Header http://www.apple.com/store, got %s", res.Header.Get("Location"))
-	}
-
-}
-
-// func TestMultiplexer(t *testing.T) {
-// 	type args struct {
-// 		w http.ResponseWriter
-// 		r *http.Request
+// 	if err != nil {
+// 		t.Fatalf("could not create request: %v", err)
 // 	}
-// 	tests := []struct {
-// 		name string
-// 		args args
-// 	}{
-// 		// TODO: Add test cases.
+
+// 	rec := httptest.NewRecorder()
+// 	PostURL(rec, req)
+
+// 	res := rec.Result()
+// 	defer res.Body.Close()
+// 	if res.StatusCode != http.StatusCreated {
+// 		t.Errorf("expected status Created; got %v", res.Status)
 // 	}
-// 	for _, tt := range tests {
-// 		t.Run(tt.name, func(t *testing.T) {
-// 			Multiplexer(tt.args.w, tt.args.r)
-// 		})
+// 	b, err := ioutil.ReadAll(res.Body)
+// 	if err != nil {
+// 		t.Fatalf("coult not read respons: %v", err)
+// 	}
+// 	if string(b) != "http://localhost:8080/a7d59904" {
+// 		t.Fatalf("expected http://localhost:8080/a7d59904; got %s", string(b))
 // 	}
 // }
+
+// func TestGetURL(t *testing.T) {
+// 	// TODO: create a record for the the URL, to that this test can work independatly
+// 	requestURL := "http://localhost:8080/a7d59904"
+// 	req, err := http.NewRequest("GET", requestURL, nil)
+
+// 	if err != nil {
+// 		t.Fatalf("could not create request: %v", err)
+// 	}
+
+// 	rec := httptest.NewRecorder()
+// 	GetURL(rec, req)
+
+// 	res := rec.Result()
+// 	defer res.Body.Close()
+// 	if res.StatusCode != http.StatusTemporaryRedirect {
+// 		t.Errorf("expected status TemporaryRedirect; got %v", res.Status)
+// 	}
+
+// 	if res.Header.Get("Location") != "http://www.apple.com/store" {
+// 		t.Errorf("Expected Header http://www.apple.com/store, got %s", res.Header.Get("Location"))
+// 	}
+
+// }
+
+// // func TestMultiplexer(t *testing.T) {
+// // 	type args struct {
+// // 		w http.ResponseWriter
+// // 		r *http.Request
+// // 	}
+// // 	tests := []struct {
+// // 		name string
+// // 		args args
+// // 	}{
+// // 		// TODO: Add test cases.
+// // 	}
+// // 	for _, tt := range tests {
+// // 		t.Run(tt.name, func(t *testing.T) {
+// // 			Multiplexer(tt.args.w, tt.args.r)
+// // 		})
+// // 	}
+// // }
