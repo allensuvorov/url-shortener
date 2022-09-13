18208f3960113728babe49f1c8ff05a69725209e
Allen <allenvs@mail.ru>
2022-09-13 18:08:42 +0300

    commented out old testing code

--- internal/app/shortner/service/url/shorten_test.go
+++ internal/app/shortner/service/url/shorten_test.go
@@ -1,51 +1,51 @@
 package url
 
-import (
-	"testing"
-)
+// import (
+// 	"testing"
+// )
 
-func TestShorten(t *testing.T) {
-	type args struct {
-		s string
-	}
-	tests := []struct {
-		name string
-		args args
-		want string
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := Shorten(tt.args.s); got != tt.want {
-				t.Errorf("Shorten() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
+// func TestShorten(t *testing.T) {
+// 	type args struct {
+// 		s string
+// 	}
+// 	tests := []struct {
+// 		name string
+// 		args args
+// 		want string
+// 	}{
+// 		// TODO: Add test cases.
+// 	}
+// 	for _, tt := range tests {
+// 		t.Run(tt.name, func(t *testing.T) {
+// 			if got := Shorten(tt.args.s); got != tt.want {
+// 				t.Errorf("Shorten() = %v, want %v", got, tt.want)
+// 			}
+// 		})
+// 	}
+// }
 
-func Test_getUniqShortHash(t *testing.T) {
-	type args struct {
-		h string
-		s string
-	}
-	tests := []struct {
-		name string
-		args args
-		want string
-	}{
-		{
-			// TODO: Add test cases.
-			name: "test shortening",
-			args: args{"1234567890", "a/b/c"},
-			want: "12345678",
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := getUniqShortHash(tt.args.h, tt.args.s); got != tt.want {
-				t.Errorf("getUniqShortHash() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
+// func Test_getUniqShortHash(t *testing.T) {
+// 	type args struct {
+// 		h string
+// 		s string
+// 	}
+// 	tests := []struct {
+// 		name string
+// 		args args
+// 		want string
+// 	}{
+// 		{
+// 			// TODO: Add test cases.
+// 			name: "test shortening",
+// 			args: args{"1234567890", "a/b/c"},
+// 			want: "12345678",
+// 		},
+// 	}
+// 	for _, tt := range tests {
+// 		t.Run(tt.name, func(t *testing.T) {
+// 			if got := getUniqShortHash(tt.args.h, tt.args.s); got != tt.want {
+// 				t.Errorf("getUniqShortHash() = %v, want %v", got, tt.want)
+// 			}
+// 		})
+// 	}
+// }
