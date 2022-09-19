557224dbe565abac1cf08355d2ec285982dc41b5
Allen <allenvs@mail.ru>
2022-09-19 14:52:21 +0300

    GetHashByURL refactored

--- internal/app/shortner/storage/storage.go
+++ internal/app/shortner/storage/storage.go
@@ -3,37 +3,36 @@ package storage
 import (
 	"log"
 
-	"github.com/allensuvorov/urlshortner/internal/app/shortner/domain/entity"
 	"github.com/allensuvorov/urlshortner/internal/app/shortner/domain/errors"
 	"github.com/allensuvorov/urlshortner/internal/app/shortner/domain/hashmap"
 )
 
 // Object with storage methods to work with DB
 type URLStorage struct {
-	InMemory *hashmap.URLHashMap
+	InMemory hashmap.URLHashMap
 }
 
 // NewURLStorage creates URLStorage object
 func NewURLStorage() *URLStorage {
 	return &URLStorage{
-		InMemory: new(hashmap.URLHashMap),
+		InMemory: make(hashmap.URLHashMap),
 	}
 }
 
 // Create adds new URL record to storage
-func (us *URLStorage) Create(ue *hashmap.URLHashMap) error {
-	us.InMemory[]
-	log.Println("Storage Create UE, appended, updated slice len is", len(us.InMemory))
-	log.Println("Storage Create UE", ue)
+func (us *URLStorage) Create(h, u string) error {
+	us.InMemory[h] = u
+	log.Println("Storage Create UE, added to map, updated map len is", len(us.InMemory))
+	log.Println("Storage Create UE, added to map, updated map is", us.InMemory)
 	return nil
 }
 
 func (us *URLStorage) GetHashByURL(u string) (string, error) {
 	log.Println("Storage GetHashByURL, looking for matching URL", u)
-	for _, v := range us.InMemory {
-		if v.URL == u {
-			log.Println("Storage GetHashByURL, found ue", v)
-			return v.Hash, nil
+	for k, v := range us.InMemory {
+		if v == u {
+			log.Println("Storage GetHashByURL, found record", k)
+			return k, nil
 		}
 	}
 	return "", errors.ErrNotFound
