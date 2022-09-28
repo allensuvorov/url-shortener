aa266255920f131e8e142713bf456ddd423ef5eb
Allen <allenvs@mail.ru>
2022-09-28 17:07:26 +0300

    SERVER_ADDRESS

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	handler "github.com/allensuvorov/urlshortner/internal/app/shortner/remote/handlers/url"
 	"github.com/allensuvorov/urlshortner/internal/app/shortner/remote/routers"
@@ -14,10 +15,12 @@ func main() {
 	// Commands to add vars to env
 	// export SERVER_ADDRESS=:8080
 	// export BASE_URL=http://localhost:8080/
+	sa := os.Getenv("SERVER_ADDRESS")
+
 	URLStorage := storage.NewURLStorage()
 	URLService := service.NewURLService(URLStorage)
 	URLHandler := handler.NewURLHandler(URLService)
 	r := routers.NewRouter(URLHandler)
-	log.Println("Serving on port: 8080")
-	log.Fatal(http.ListenAndServe(":8080", r))
+	log.Println("Serving on port", sa)
+	log.Fatal(http.ListenAndServe(sa, r))
 }
