bddebb74aff8ae3e4a9c759a7358eeb0ecc5fc87
Allen <allenvs@mail.ru>
2022-10-04 15:15:26 +0600

    basic writing to file added, let's see if it works

--- internal/app/shortner/storage/file.go
+++ internal/app/shortner/storage/file.go
@@ -2,14 +2,14 @@ package storage
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"os"
 )
 
 // Write to file
 type writer struct {
-	file    *os.File
-	encoder *json.Encoder
+	file *os.File
 }
 
 func NewWriter(fileName string) (*writer, error) {
@@ -18,8 +18,7 @@ func NewWriter(fileName string) (*writer, error) {
 		return nil, err
 	}
 	return &writer{
-		file:    file,
-		encoder: json.NewEncoder(file),
+		file: file,
 	}, nil
 }
 func (p *writer) WriteEvent(event *Event) error {
@@ -31,16 +30,21 @@ func (p *writer) Close() error {
 
 func write(h, u, fsp string) error {
 	log.Printf("Storage: saving to path - %s", fsp)
-	// path will be /Users/allen/go/src/yandex/projects/urlshortner/internal/app/shortner/storage
+
+	// Create and open file
 	fileName := "urls.txt"
-	writer, err := NewWriter(fileName)
+	file, err := os.OpenFile(fsp+fileName, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0777)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer writer.Close()
+	// Close file at the end
+	defer file.Close()
 
-	if err := writer.WriteEvent(event); err != nil {
-		log.Fatal(err)
+	// Write to file
+	enc := json.NewEncoder(file)                                // will be encoding to file
+	if err := enc.Encode(map[string]string{h: u}); err != nil { // add map to buff
+		fmt.Println(err)
+		return nil
 	}
 
 	return nil
