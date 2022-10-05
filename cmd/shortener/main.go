11c8a01a3a3f60439e31a42fc8a003fc5881790d
Allen <allenvs@mail.ru>
2022-10-06 00:47:19 +0600

    port can now be set by flag

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 	"os"
@@ -11,18 +12,35 @@ import (
 	"github.com/allensuvorov/urlshortner/internal/app/shortner/storage"
 )
 
-func main() {
+var (
+	port *string
+	bu   *string
+	fsp  *string
+)
 
-	sa, ok := os.LookupEnv("SERVER_ADDRESS")
-	if !ok {
-		log.Printf("%s not set\n; passing default", "SERVER_ADDRESS")
-		sa = ":8080"
+func init() {
+	// get server address from CLI flag
+	port = flag.String("a", ":8080", "SERVER_ADDRESS")
+	// bu = flag.String("b", ":8080", "SERVER_ADDRESS")
+}
+
+func main() {
+	flag.Parse()
+	log.Println("Port from Flag is", *port)
+	// get server address from local env if not in cli flags
+	if len(*port) == 0 {
+		sa, ok := os.LookupEnv("SERVER_ADDRESS")
+		if !ok {
+			log.Printf("%s not set\n; passing default", "SERVER_ADDRESS")
+			sa = ":8080"
+		}
+		*port = sa
 	}
 
 	URLStorage := storage.NewURLStorage()
 	URLService := service.NewURLService(URLStorage)
 	URLHandler := handler.NewURLHandler(URLService)
 	r := routers.NewRouter(URLHandler)
-	log.Println("Serving on port", sa)
-	log.Fatal(http.ListenAndServe(sa, r))
+	log.Println("Serving on port", *port)
+	log.Fatal(http.ListenAndServe(*port, r))
 }
