8a260780740acd6c093f18efc4f0650d8399a03f
Allen <allenvs@mail.ru>
2022-10-07 18:14:13 +0600

    config delivers CLI flag data for SA

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -19,9 +19,7 @@ func main() {
 	URLHandler := handler.NewURLHandler(URLService)
 	r := routers.NewRouter(URLHandler)
 	log.Println("Main: now will try to get sa from config")
-	// sa := config.UC.SA // server address
-	defaultSA := ":8080"
-	sa := &defaultSA
+	sa := config.UC.SA // server address from config
 	log.Println("Serving on port", *sa)
 	log.Fatal(http.ListenAndServe(*sa, r))
 }
