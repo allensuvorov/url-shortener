3296e902c1fb82e915e9323a8a1bff2c243aa165
Allen <allenvs@mail.ru>
2022-10-13 19:43:29 +0600

    figuring out type and interplay of all pieces

--- internal/app/shortner/middleware/compress/compress.go
+++ internal/app/shortner/middleware/compress/compress.go
@@ -18,7 +18,7 @@ func (w gzipWriter) Write(b []byte) (int, error) {
 	return w.Writer.Write(b)
 }
 
-// Reader
+// gzipReader is a Readcloser. As is r.body
 type gzipReader struct {
 	rc  io.ReadCloser
 	gzr *gzip.Reader
@@ -38,26 +38,55 @@ func (g gzipReader) Close() error {
 type GzipHandler struct {
 }
 
-// middleware принимает параметром Handler и возвращает тоже Handler.
+/* Logic of GzipMiddleware
+
+- We are trying to give our server ability to decompress and compress data exchange.
+- To do that we will add a middleware - GzipMiddleware - between router and handlers.
+- GzipMiddleware "wraps" our handlers (“оборачивает” обработчики) to add gzip encoding to the flow.
+
+- To do that GzipMiddleware takes:
+	- our handler with it's arguments:
+		- ResponseWriter - type interface - any objects with a required set of functionality.
+		- pointer to a Request struct, with fields including:
+			- url - path we can route to call the right handler
+			- header - the list of paramenter, including encoding headers.
+			- body - type interface io.ReadCloser ()
+- Body carries the primary payload of the request, and we are going to decompress the payload.
+- Body is any object with ReadCloser functionality. ReadCloser is the interface that groups the basic Read and Close methods.
+- To decompress the data in body, we need to
+	- read the data,
+	- decompress this data,
+	- create new body,
+	- pass decompressed data to the new body,
+	- close body reader.
+
+- To that we will build an object - a struct, that has read and close methods.
+
+- returns a new handler function that calls our handler, with it's method ServeHTTP.
+- In that call it passes updated arguments to that handler
+*/
+
+// GzipMiddleware принимает параметром Handler и возвращает тоже Handler.
 func (g GzipHandler) GzipMiddleware(next http.Handler) http.Handler {
 	// собираем Handler приведением типа
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		log.Println("Compress/GzipMiddleware: Hi, I'm GzipMiddleware ")
 
 		// check if request is gzip-encoded
-		// if strings.Contains(r.Header.Get("Content-Encoding"), "gzip") {
-		// 	log.Println("Compress/Middleware: POST request Content-Encoding == gzip")
-		// 	// TODO: read response
+		if strings.Contains(r.Header.Get("Content-Encoding"), "gzip") {
+			log.Println("Compress/Middleware: POST request Content-Encoding == gzip")
 
-		// 	gz, _ := gzip.NewReader(r.Body)
-		// 	gzr := gzipReader{
-		// 		rc:  r.Body,
-		// 		gzr: gz,
-		// 	}
-		// 	r.Body = gzr
-		// 	defer gz.Close()
+			// создаём *gzip.Reader, который будет читать тело запроса
+			// и распаковывать его
+			gz, _ := gzip.NewReader(r.Body) // *Reader - type Reader struct {Header}
+			gzr := gzipReader{              // new ReadCloser
+				rc:  r.Body, // field rc gets the original request.Body - also a ReadCloser
+				gzr: gz,     // *gzip.Reader
+			}
+			r.Body = gzr // struct that is ReadCloser
+			defer gz.Close()
 
-		// }
+		}
 
 		// check if sender can accept is gzip-encoded response
 		if strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
@@ -83,9 +112,9 @@ func (g GzipHandler) GzipMiddleware(next http.Handler) http.Handler {
 
 			log.Println("Compress/Middleware: gzip not accepted, header:", r.Header["Accept-Encoding"])
 
-			// замыкание — используем ServeHTTP следующего хендлера
-			next.ServeHTTP(w, r)
 		}
+		// замыкание — используем ServeHTTP следующего хендлера
+		next.ServeHTTP(w, r)
 
 		log.Println("Compress/GzipMiddleware: Bye! ")
 	})
