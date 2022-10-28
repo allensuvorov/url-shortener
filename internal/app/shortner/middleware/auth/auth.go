dc00d6777055415e27b5434348530a7ce06ebcf3
Allen <allenvs@mail.ru>
2022-10-28 17:00:10 +0600

    auth: const for registered string

--- internal/app/shortner/middleware/auth/auth.go
+++ internal/app/shortner/middleware/auth/auth.go
@@ -6,16 +6,18 @@ import "net/http"
 //TODO generate signature for the client ID
 //TODO read/write cookie
 
+const registered = "registered"
+
 func AuthMiddleware(next http.Handler) http.Handler {
 	// собираем Handler приведением типа
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		cookie, err := r.Cookie("registered")
+		cookie, err := r.Cookie(registered)
 
 		// no cookie
 		if err == http.ErrNoCookie {
 			// TODO generate ID with signature set it to cookie
 			cookie = &http.Cookie{
-				Name:  "registered",
+				Name:  registered,
 				Value: "0",
 			}
 		}
