c72d29ed19fb890e034fe8517ccedb342819fe23
Allen <allenvs@mail.ru>
2022-10-28 16:35:23 +0600

    auth: reading Cookie - registerred

--- internal/app/shortner/middleware/auth/auth.go
+++ internal/app/shortner/middleware/auth/auth.go
@@ -1,14 +1,14 @@
 package auth
 
+import "net/http"
+
 //TODO generate client ID
 //TODO generate signature for the client ID
 //TODO read/write cookie
 
-/*
 func AuthMiddleware(next http.Handler) http.Handler {
 	// собираем Handler приведением типа
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-
-	}
+		cookie, err := r.Cookie("registered")
+	})
 }
-*/
