687741c88e4c893ff51816fd469e20cb6dca97fb
Allen <allenvs@mail.ru>
2022-10-28 16:56:14 +0600

    auth: Cookie cases

--- internal/app/shortner/middleware/auth/auth.go
+++ internal/app/shortner/middleware/auth/auth.go
@@ -10,5 +10,19 @@ func AuthMiddleware(next http.Handler) http.Handler {
 	// собираем Handler приведением типа
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		cookie, err := r.Cookie("registered")
+
+		// no cookie
+		if err == http.ErrNoCookie {
+			// TODO generate ID with signature set it to cookie
+			cookie = &http.Cookie{
+				Name:  "registered",
+				Value: "0",
+			}
+		}
+
+		// cookie - no reg field
+		// cookie, reg value, wrong key
+		// cookie, reg value, write key
+
 	})
 }
