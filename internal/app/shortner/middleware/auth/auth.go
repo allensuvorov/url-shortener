5f95c0b638a13f9d909e5d4ef7a08e881acdcb07
Allen <allenvs@mail.ru>
2022-10-28 15:45:43 +0600

    auth: TODOs

--- internal/app/shortner/middleware/auth/auth.go
+++ internal/app/shortner/middleware/auth/auth.go
@@ -1,1 +1,14 @@
 package auth
+
+//TODO generate client ID
+//TODO generate signature for the client ID
+//TODO read/write cookie
+
+/*
+func AuthMiddleware(next http.Handler) http.Handler {
+	// собираем Handler приведением типа
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+	}
+}
+*/
