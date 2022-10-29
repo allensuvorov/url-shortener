8c08e7d312ef6d6690e32d1904c8556db3bd1636
Allen <allenvs@mail.ru>
2022-10-29 21:58:18 +0600

    URLEntity, ClientEntity

--- internal/app/shortner/domain/entity/entity.go
+++ internal/app/shortner/domain/entity/entity.go
@@ -1,7 +1,14 @@
 package entity
 
 type URLEntity struct {
-	user string
-	hash string
-	path string
+	client int // foreign key
+	hash   string
+	url    string
+}
+
+type ClientEntity struct {
+	id              int
+	cookieID        string
+	cookieSignature string
+	key             string
 }
