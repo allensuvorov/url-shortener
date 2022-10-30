dad5147b2fa0fc4f3e365d6fb2d9d62d93224a3a
Allen <allenvs@mail.ru>
2022-10-30 22:49:25 +0600

    back to adding UserActivity in InMemory struct

--- internal/app/shortner/storage/storage.go
+++ internal/app/shortner/storage/storage.go
@@ -10,20 +10,20 @@ import (
 
 // Object with storage methods to work with DB
 type URLStorage struct {
-	InMemory hashmap.URLHashMap
+	InMemory InMemory
 }
 
-// type InMemory struct {
-// 	URLHashMap   hashmap.URLHashMap
-// 	UserActivity hashmap.UserActivity
-// }
+type InMemory struct {
+	URLHashMap   hashmap.URLHashMap
+	UserActivity hashmap.UserActivity
+}
 
 // NewURLStorage creates URLStorage object
 func NewURLStorage() *URLStorage {
 	// Restore data at start up
 	fsp := config.UC.FSP
 	um := make(hashmap.URLHashMap) // url map
-	// ua := make(hashmap.UserActivity)
+	ua := make(hashmap.UserActivity)
 
 	// TODO restore from file both um and ua
 
@@ -33,16 +33,16 @@ func NewURLStorage() *URLStorage {
 	}
 
 	return &URLStorage{
-		InMemory: um,
+		InMemory: InMemory{um, ua},
 	}
 }
 
 // Create adds new URL record to storage
 func (us *URLStorage) Create(h, u string) error {
 	// Save to map
-	us.InMemory[h] = u
-	log.Println("Storage/Create(): added to map, updated map len is", len(us.InMemory))
-	log.Println("Storage/Create(): added to map, updated map is", us.InMemory)
+	us.InMemory.URLHashMap[h] = u
+	log.Println("Storage/Create(): added to map, updated map len is", len(us.InMemory.URLHashMap))
+	log.Println("Storage/Create(): added to map, updated map is", us.InMemory.URLHashMap)
 
 	// get file storage path from config
 	fsp := config.UC.FSP
@@ -57,7 +57,7 @@ func (us *URLStorage) Create(h, u string) error {
 
 func (us *URLStorage) GetHashByURL(u string) (string, error) {
 	log.Println("Storage/GetHashByURL(), looking for matching URL", u)
-	for k, v := range us.InMemory {
+	for k, v := range us.InMemory.URLHashMap {
 		if v == u {
 			log.Println("Storage GetHashByURL, found record", k)
 			return k, nil
@@ -67,9 +67,9 @@ func (us *URLStorage) GetHashByURL(u string) (string, error) {
 }
 
 func (us *URLStorage) GetURLByHash(h string) (string, error) {
-	log.Println("Storage/GetURLByHash(), looking in map len", len(us.InMemory))
+	log.Println("Storage/GetURLByHash(), looking in map len", len(us.InMemory.URLHashMap))
 	log.Println("Storage/GetURLByHash(), looking for matching Hash", h)
-	u, ok := us.InMemory[h]
+	u, ok := us.InMemory.URLHashMap[h]
 	if !ok {
 		return "", errors.ErrNotFound
 	}
