23f21fd9da478212d4bff27057d32f28f72a8381
Allen <allenvs@mail.ru>
2022-11-20 22:37:07 +0600

    compress_test.go removed comments

--- internal/app/shortner/middleware/compress/compress_test.go
+++ internal/app/shortner/middleware/compress/compress_test.go
@@ -19,7 +19,6 @@ import (
 
 func TestGzipHandler_GzipMiddleware(t *testing.T) {
 
-	// Create Handler
 	config.BuildConfig()
 	usm := storage.NewURLStorage()
 	us := service.NewURLService(usm)
@@ -28,8 +27,8 @@ func TestGzipHandler_GzipMiddleware(t *testing.T) {
 	tests := []struct {
 		name                     string
 		url                      string
-		headerAcceptEncoding     string // Accept-Encoding
-		headerContentEncoding    string // Content-Encoding
+		headerAcceptEncoding     string
+		headerContentEncoding    string
 		expectedResponseBody     []byte
 		expectedResponseCEHeader string
 	}{
@@ -60,18 +59,14 @@ func TestGzipHandler_GzipMiddleware(t *testing.T) {
 				log.Fatalf("failed compress data: %v", err)
 			}
 
-			// Create POST request
 			r := httptest.NewRequest(http.MethodPost, "http://localhost:8080", &buf)
 			w := httptest.NewRecorder()
 
-			// Add headers
 			r.Header.Set("Content-Encoding", tt.headerAcceptEncoding)
 			r.Header.Set("Accept-Encoding", tt.headerAcceptEncoding)
 
-			// Handler wrapped in middleware
-			h := GzipMiddleware(http.HandlerFunc(uh.Create)) // h - is a struct
+			h := GzipMiddleware(http.HandlerFunc(uh.Create))
 
-			// Call Middleware
 			h.ServeHTTP(w, r)
 			log.Println("compress_test: statuscode", w.Code)
 
@@ -81,7 +76,6 @@ func TestGzipHandler_GzipMiddleware(t *testing.T) {
 				log.Fatal(err)
 			}
 
-			// при чтении вернётся распакованный слайс байт
 			body, err := io.ReadAll(zr)
 			if err != nil {
 				http.Error(w, err.Error(), http.StatusInternalServerError)
